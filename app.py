15ccae79fd4e6bfbdf2508bbe1d7b05484a0f5dc
Mega987 <secondmega116@gmail.com>
2024-08-06 14:13:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import requests
 def fetch_poster(movie_id):
     response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=bc4eb15169a481ca444b8342939febee&language=en-US'.format(movie_id))
     data = response.json()
-    return "https://image.tmdb.org/t/p/w500/"+data['poster_path']
+    return "https://image.tmdb.org/t/p/w500"+data['poster_path']
     
 
 
